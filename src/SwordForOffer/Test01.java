a0e37fa7f9383f29b20a679f7f6fab9ba2d4c27c
Noble777 <guolei1.chen@gmail.com>
2018-12-20 19:13:41 -0500

    sfo2

--- src/SwordForOffer/Test01.java
+++ src/SwordForOffer/Test01.java
@@ -2,6 +2,9 @@ package SwordForOffer;
 
 /**
  * Created by apple on 12/20/18.
+ * 赋值运算
+ * 1.对于传入的参数，是不应该被修改的，所以使用final修饰
+ * 2.此赋值从左到右进行，a=b=c等价于a=c，b不会被赋值；
  */
 public class Test01 {
     public static class MyString {
@@ -32,6 +35,9 @@ public class Test01 {
         MyString s1 = new MyString("a");
         MyString s2 = new MyString("b");
         MyString s3 = new MyString("c");
+        System.out.println("s1:" + s1);
+        System.out.println("s2:" + s2);
+        System.out.println("s3:" + s3);
         System.out.println(s1.assign(s2).assign(s3));
         System.out.println("s1:" + s1);
         System.out.println("s2:" + s2);

f9b2d6848299198366aa17c3dbb45168b4d05e7d
Noble777 <guolei1.chen@gmail.com>
2019-01-01 19:13:08 -0500

    update 1.1

--- src/SwordForOffer/TraversalOfBinaryTree.java
+++ src/SwordForOffer/TraversalOfBinaryTree.java
@@ -1,10 +1,7 @@
 package SwordForOffer;
 
 import javax.swing.tree.TreeNode;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Created by apple on 12/30/18.
@@ -17,6 +14,17 @@ public class TraversalOfBinaryTree {
         TreeNode(int x) {
             val = x;
         }
+    }
+
+    public static List<Integer> preorderRecursively(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        res.add(root.val);
+        res.addAll(preorderRecursively(root.left));
+        res.addAll(preorderRecursively(root.right));
+        return res;
     }
 
     public static List<Integer> preorderIteratively(TreeNode root) {
@@ -41,6 +49,16 @@ public class TraversalOfBinaryTree {
         return res;
     }
 
+    public static List<Integer> inorderRecursively(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        res.addAll(inorderRecursively(root.left));
+        res.add(root.val);
+        res.addAll(inorderRecursively(root.right));
+        return res;
+    }
 
     public static List<Integer> inorderIteratively(TreeNode root) {
         Stack<TreeNode> stack = new Stack<>();
@@ -62,6 +80,17 @@ public class TraversalOfBinaryTree {
         return res;
     }
 
+
+    public static List<Integer> postorderRecursively(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        res.addAll(postorderRecursively(root.left));
+        res.addAll(postorderRecursively(root.right));
+        res.add(root.val);
+        return res;
+    }
 
     // 左右中
     public static List<Integer> postorderIteratively(TreeNode root) {
@@ -94,6 +123,32 @@ public class TraversalOfBinaryTree {
         return res;
     }
 
+    // level order
+    public static List<List<Integer>> levelorder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int qz = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < qz; i++) {
+                TreeNode cur = queue.poll();
+                list.add(cur.val);
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            res.add(new ArrayList<>(list));
+        }
+        return res;
+    }
+
     public static void main(String[] args){
         //            1
         //              \
@@ -104,19 +159,20 @@ public class TraversalOfBinaryTree {
         TreeNode root = new TreeNode(1);
         root.right = new TreeNode(2);
         root.right.left = new TreeNode(3);
-//        List<Integer> list_preorderRecursively = preorderRecursively(root);
-//        System.out.print("preorderRecursively: "+'\t');
-//        System.out.println(list_preorderRecursively.toString());
-//
-//        List<Integer> list_inorderRecursively = inorderRecursively(root);
-//        System.out.print("inorderRecursively: "+'\t');
-//        System.out.println(list_inorderRecursively.toString());
-//
-//        List<Integer> list_postorderRecursively = postorderRecursively(root);
-//        System.out.print("postorderRecursively: "+'\t');
-//        System.out.println(list_postorderRecursively.toString());
-//        System.out.println();
+        List<Integer> list_preorderRecursively = preorderRecursively(root);
+        System.out.print("preorderRecursively: "+'\t');
+        System.out.println(list_preorderRecursively.toString());
 
+        List<Integer> list_inorderRecursively = inorderRecursively(root);
+        System.out.print("inorderRecursively: "+'\t');
+        System.out.println(list_inorderRecursively.toString());
+
+        List<Integer> list_postorderRecursively = postorderRecursively(root);
+        System.out.print("postorderRecursively: "+'\t');
+        System.out.println(list_postorderRecursively.toString());
+        System.out.println();
+
+        System.out.println("--------------");
 
         List<Integer> list_preorderIteratively = preorderIteratively(root);
         System.out.print("preorderIteratively: "+'\t');
@@ -131,9 +187,9 @@ public class TraversalOfBinaryTree {
         System.out.println(list_postorderIteratively.toString());
         System.out.println();
 
-//        List<Integer> list_levelorder = levelorder(root);
-//        System.out.print("levelorder: "+'\t');
-//        System.out.println(list_levelorder.toString());
+        List<List<Integer>> list_levelorder = levelorder(root);
+        System.out.print("levelorder: "+'\t');
+        System.out.println(list_levelorder.toString());
     }
 
 
